7de9c9024be37ef3bcd6e3685fc50dcc92f22769
dharaprayagraj <dharaprayagraj@gmail.com>
2023-10-21 22:17:38 -0400

    Execute tests in Docker using Selenium Grid

---
+++ ep-10-selenium-grid-using-docker/SeleniumGridUsingDocker/README.md
@@ -0,0 +1,18 @@
+# colorstreet automation
+
+Instructions to run this project.
+
+1. Install Eclipse Version 2020-06 (4.16.0)
+2. Install JDK version 1.8.0_251
+3. Setup Java Environment variable
+4. Launch Eclipse
+5. Create Workspace in Eclipse
+6. Clone this repository.
+7. Import this project in Eclipse.
+8. Download latest chromedriver and place it in this folder path `C://drivers/chromedriver.exe` and it should match with your installed chrome browser version.
+9. All the dependences will be imported automatically from Maven repositories.
+10. Go to `testng.xml` file.
+11. Right click and Run as `TestNG Suite`.
+
+![image](https://user-images.githubusercontent.com/117222599/199352733-a77df684-1bdd-4e3c-af5d-ebf89a4b856c.png)
+
---
+++ ep-10-selenium-grid-using-docker/SeleniumGridUsingDocker/src/test/java/Newsletter/ChromeSeleniumGrid.java
@@ -0,0 +1,50 @@
+package Newsletter;
+
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class ChromeSeleniumGrid {
+
+	public WebDriver driver;
+	@BeforeMethod
+	public void setUp() throws MalformedURLException
+	{	
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
+		driver.get("https://www.google.com/");
+	}
+	
+	@Test
+	@Parameters({ "browser"})
+	public void testmethodA(String browser)
+	{	
+		System.out.println("BrowserName: "+ browser);		
+	}
+		
+	@AfterMethod
+	public void discontinueBrowser()
+	{
+		driver.close();
+	}
+
+
+}
